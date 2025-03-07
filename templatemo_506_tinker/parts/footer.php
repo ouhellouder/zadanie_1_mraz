98a48c8979fa380859f3df485674bd70b2c32cf5
ouhellouder <78954248+ouhellouder@users.noreply.github.com>
2025-03-07 14:23:07 +0100

    Add files via upload
    Main

---
+++ templatemo_506_tinker/parts/footer.php
@@ -0,0 +1,43 @@
+<footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 col-sm-12">
+                    <div class="logo">
+                        <a class="logo-ft scroll-top" href="#"><em>T</em>inker</a>
+                        <p>Copyright &copy; 2020 Your Company 
+                       <br>Design: TemplateMo</p>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-12">
+                    <div class="location">
+                        <h4>Location</h4>
+                        <ul>
+                            <li>46 Raffles Ave, <br>Singapore 039803</li>
+                            <li>12 Republic Blvd, <br>Singapore 038975</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-2 col-sm-12">
+                    <div class="contact-info">
+                        <h4>More Info</h4>
+                        <ul>
+                            <li><em>Phone</em>: 040-040-0440</li>
+                            <li><em>Email</em>: tk@company.co</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-2 col-sm-12">
+                    <div class="connect-us">
+                        <h4>Get Social with us</h4>
+                        <ul>
+                            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                            <li><a href="#"><i class="fa fa-google"></i></a></li>
+                            <li><a href="#"><i class="fa fa-rss"></i></a></li>
+                            <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
